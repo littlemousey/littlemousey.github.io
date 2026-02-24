fcad26c268d810772f179e3cdb0bf555d14c0586
Ans de Nijs <littlemousey@users.noreply.github.com>
2026-02-24 21:55:40 +0100

    feat: add volunteer section

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -94,6 +94,8 @@ export const skills = {
   frontendDevelopment: [
     "Reactjs",
     "Redux",
+    "Zustand",
+    "Remix",
     "Vue.js",
     "Nuxt.js",
     "Angular",
@@ -118,86 +120,11 @@ export const skills = {
     "Sentry",
     "MUI",
     "Jest/Jasmine",
-    "Bootstrap"
+    "Bootstrap",
+    "Visual Studio Code",
+    "Github co-pilot"
   ],
 };
-
-// export const projects = [
-//   {
-//     title: "Net Zero Carbon Emissions",
-//     github: "https://github.com/rishikesh2003/Prodigi",
-//     description: [
-//       "WiFi-RTT: Developed indoor occupancy tracking for energy optimization.",
-//       "IoT Solutions: Implemented smart monitoring for energy efficiency and food waste reduction.",
-//       "Real-Time Tracking: Designed systems to monitor carbon emissions and optimize resources.",
-//       "Data-Driven Insights: Analyzed user patterns for adaptive energy and food management.",
-//       "Reward Integration: Built QR-based green points system to incentivize eco-friendly actions.",
-//     ],
-//   },
-//   {
-//     title: "Mental Aarog",
-//     github: "https://github.com/rishikesh2003/mental-aarog",
-//     description: [
-//       "A holistic mental health app leveraging AI and blockchain for early detection and personalized solutions for depression.",
-//       "Features include social media analysis (ML-based sentiment tracking), PHQ-9 assessments, smartwatch integration for sleep and activity data, guided meditation, and smart suggestions for food, travel, music, and movies.",
-//       "Developed a rewards system using MAG crypto token on Ethereum, enabling user engagement through in-app incentives.",
-//       "Secure storage implemented via web3.storage, IPFS, and FileCoin.",
-//       "Built with React, Supabase, Node.js, Flask, and Solidity, showcasing seamless integration of health tech and blockchain.",
-//     ],
-//   },
-// ];
-
-export const awards = [
-  {
-    name: "IEEE YESIST12 Hackathon",
-    issuer: "IEEE",
-    date: "Sep 2022",
-    type: "International",
-    position: "Second Place",
-  },
-  {
-    name: "Prodigi Cognizant Hackathon",
-    issuer: "Cognizant",
-    date: "Feb 2023",
-    type: "National",
-    position: "Second Runner-up",
-  },
-  {
-    name: "Cisco Thingqbator Hackathon",
-    issuer: "Cisco",
-    date: "Jan 2023",
-    type: "National",
-    position: "First Runner-up",
-  },
-  {
-    name: "Innovators Day",
-    issuer: "Sri Manakula Vinayagar Engineering College, Pondicherry",
-    date: "Sep 2022",
-    type: "National",
-    position: "First Prize",
-  },
-  {
-    name: "KG Hackfest'22",
-    issuer: "KGiSL Institute of Technology, Coimbatore",
-    date: "Sep 2022",
-    type: "National",
-    position: "Second Prize",
-  },
-  {
-    name: "Innohacks'22",
-    issuer: "Innogeeks, KIET Group of Institutions, New Delhi",
-    date: "May 2022",
-    type: "National",
-    position: "Second Runner-up",
-  },
-  {
-    name: "Hack @ SKCET",
-    issuer: "Hackclub SKCET, SKCET, Coimbatore",
-    date: "Feb 2022",
-    type: "National",
-    position: "Most Impactful Hack",
-  },
-];
 
 export const articles = [
   {
@@ -217,5 +144,14 @@ export const articles = [
     description: 'Interview as female developer',
     url: 'https://rush.nl/zo-is-echt-om-als-vrouw-tech-industrie-werken/',
     date: '2018'
+  },
+];
+
+export const volunteerExperiences = [
+  {
+    organisation: 'World Wildlife Fund',
+    description: 'Organizing and preparing educational nature activities for children',
+    period: '2023-2025',
+    url: 'https://www.wwf.nl/kom-in-actie/met-tijd/word-vrijwilliger'
   },
 ];
